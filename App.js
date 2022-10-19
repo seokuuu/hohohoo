834ebb39d8d0e76de3f54f3267900bffb6f5beff
seokuuu <114736988+seokuuu@users.noreply.github.com>
2022-10-19 15:18:28 +0900

    Revert "호호호"
    This reverts commit e4c18631dba73cbb1de5d0cd30dbd5c04546a337.

--- App.js
+++ App.js
@@ -1,17 +1,24 @@
-/* eslint-disable */
-import React from "react";
-import "./App.css";
-import TodoList from "./components/pages/TodoList";
+/* eslint-disable */ 
+import React from 'react';
+import './App.css';
+import TodoList from './components/pages/TodoList';
 
-function App() {
-  // 집으로 가져오기
+function App(){
+
+
+
+// 집으로 가져오기
   return (
     <div className="App">
       <p>안녕하세요 1조입니다.</p>
-      <p>안녕하세요 호호호.</p>
       <TodoList></TodoList>
-    </div>
+      </div>
+
   );
-}
+  }
+
+
+
+
 
 export default App;
