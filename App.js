3c9caca4b13f5a660876288a537c5834c48ab2f9
NyeongDev <90minyeong90@gmail.com>
2022-10-19 14:56:42 +0900

    conflict test

--- App.js
+++ App.js
@@ -1,24 +1,16 @@
-/* eslint-disable */ 
-import React from 'react';
-import './App.css';
-import TodoList from './components/pages/TodoList';
+민영;
+import React from "react";
+import "./App.css";
+import TodoList from "./components/pages/TodoList";
 
-function App(){
-
-
-
-// 집으로 가져오기
+function App() {
+  // 집으로 가져오기
   return (
     <div className="App">
       <p>안녕하세요 1조입니다.</p>
       <TodoList></TodoList>
-      </div>
-
+    </div>
   );
-  }
-
-
-
-
+}
 
 export default App;
